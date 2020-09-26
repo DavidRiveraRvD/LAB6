5704305136a3fd87f81b87d924d87c2b2fcdb3ad
StivenVanegas <nevits28@hotmail.com>
2020-09-25 21:56:50 -0500

    avance

--- servlet/src/main/webapp/intro.js
+++
@@ -1,9 +0,0 @@
-<script>
-  function validateForm() {
-  var x = document.forms["formulario"]["id"].value;
-  if (x != parseInt(x)) {
-    alert("Ingrese un numero valido");
-    return false;
-  }
-}
-</script>
